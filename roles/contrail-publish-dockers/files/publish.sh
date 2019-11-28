4a4738eecb6f017bd64eb9e93f111f0bc712736c
alexey-mr <alexey.morlang@gmail.com>
2019-11-28 15:02:43 +0300

    Fixed publishing

--- roles/contrail-publish-dockers/files/publish.sh
+++ roles/contrail-publish-dockers/files/publish.sh
@@ -136,7 +136,7 @@ function do_publish() {
   local container=$1
 
   local full_name_list=$(get_container_full_name_list $container $CONTAINER_TAG)
-  if [[ "$?" != "0" || -z "$full_name" ]] ; then
+  if [[ "$?" != "0" || -z "$full_name_list" ]] ; then
     warn "$container skipped"
     return 0
   fi
